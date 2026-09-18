232eb3966bd4510ce3244f2909fa4662a3a7b612
SALAS KS <salasks.official@gmail.com>
2026-09-18 23:46:15 +0530

    fix: remove sqlite3 require

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const bcrypt = require('bcryptjs');
 
